3280913387434815e35e7cd5dab337a33732cdfc
Jessica Swenson <jessicaswenson@jessicas-mbp-2.lan>
2022-07-07 21:20:27 -0600

    added imgs to game page

--- client/gamepage.js
+++ client/gamepage.js
@@ -5,13 +5,27 @@ axios.get('http://localhost:4004/api/gameNamePlace')
     .then((res) => {
         console.log(res.data)
         const { potatoName, randomPlace } = res.data;
-        nameHeader.innerHTML = `<h3>Hello ${potatoName} let's go to the ${randomPlace.places_name} </h3>`
+        nameHeader.innerHTML = `<h3>${potatoName} Potato is going to the ${randomPlace.places_name} </h3>`
     })
     .catch((err) => console.log(err));
 
 axios.get('http://localhost:4004/api/gameAccessories')
     .then((res) => {
-        console.log(res.data);
+        // console.log(res.data[0]);
+        res.data.forEach((accessories) => {
+            const accessoriesImg = document.createElement('img');
+            accessoriesImg.src = accessories.accessories_url;
+            accessoriesImg.classList.add('accessories-pic');
+            accessoriesContainer.appendChild(accessoriesImg);
+
+            // accessoriesContainer.innerHTML = `<img src="${accessories.accessories_url}" alt="${accessories.accessories_name}">`
+        })
+
+        // for (let i = 0; i < 24; i++) {
+        //     console.log(res.data[i]);
+        //     accessoriesContainer.innerHTML = `<img src="${res.data[i].accessories_url}" alt="${res.data[i].accessories_name}">`
+        // }
+
 
     })
 
