2f6ee4b321589e38156a23e9d622f412753ed9d7
Jessica Swenson <jessicaswenson@jessicas-mbp-2.lan>
2022-07-08 22:26:07 -0600

    added potato and some styling

--- client/gamepage.js
+++ client/gamepage.js
@@ -1,6 +1,8 @@
 const nameHeader = document.querySelector('#name-header');
-const accessoriesContainer = document.querySelector('#accessories-container')
+const accessoriesContainer = document.querySelector('#accessories-container');
+const accessoriesPic = document.querySelector('#accessories-pic')
 
+//gets the name and (place from database)
 axios.get('http://localhost:4004/api/gameNamePlace')
     .then((res) => {
         console.log(res.data)
@@ -9,6 +11,7 @@ axios.get('http://localhost:4004/api/gameNamePlace')
     })
     .catch((err) => console.log(err));
 
+//gets accessories from database
 axios.get('http://localhost:4004/api/gameAccessories')
     .then((res) => {
         // console.log(res.data[0]);
@@ -18,5 +21,10 @@ axios.get('http://localhost:4004/api/gameAccessories')
             accessoriesImg.classList.add('accessories-pic');
             accessoriesContainer.appendChild(accessoriesImg);
         })
-    })
+    });
+
+
+//add an eventlistener that is looking for a click and moves it to another box
+
+
 
