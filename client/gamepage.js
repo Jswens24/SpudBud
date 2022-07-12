205e547c964839c1055610875205f4c4df172e1f
Jessica Swenson <jessicaswenson@jessicas-mbp-2.lan>
2022-07-11 19:34:53 -0600

    minor updates on css

--- client/gamepage.js
+++ client/gamepage.js
@@ -1,13 +1,14 @@
 const nameHeader = document.querySelector('#name-header');
 const accessoriesContainer = document.querySelector('#accessories-container');
-const accessoriesPic = document.querySelector('#accessories-pic')
+const accessoriesPic = document.querySelectorAll('img.accessories-pic');
+const selectedItemOne = document.querySelector('.item-one');
 
 //gets the name and (place from database)
 axios.get('http://localhost:4004/api/gameNamePlace')
     .then((res) => {
         console.log(res.data)
         const { potatoName, randomPlace } = res.data;
-        nameHeader.innerHTML = `<h3>${potatoName} Potato is going to the ${randomPlace.places_name} </h3>`
+        nameHeader.innerHTML = `<h3>${potatoName} Potato is going ${randomPlace.places_name}! </h3>`
     })
     .catch((err) => console.log(err));
 
@@ -24,7 +25,21 @@ axios.get('http://localhost:4004/api/gameAccessories')
     });
 
 
+
+//front end functions 
+
+const moveToSelectedItem = (evt) => {
+    console.log(evt.target);
+}
+
+
+
 //add an eventlistener that is looking for a click and moves it to another box
 
+for (i = 0; i < accessoriesPic.length; i++) {
+    accessoriesPic.addEventListener('click', (evt) => {
+        console.log(evt);
+    })
+}
 
 
