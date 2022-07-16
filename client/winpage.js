1c715c1492cd7d74a4213b81bcf9a66d5656e580
Jessica Swenson <jessicaswenson@jessicas-mbp-2.lan>
2022-07-16 09:00:11 -0600

    added accessories to win page

--- client/winpage.js
+++ client/winpage.js
@@ -1,12 +1,13 @@
 const winHeader = document.querySelector('.win-header');
 const accessoriesContainer = document.querySelector('.accessories-container');
 
-let randomPlace = null;
+let randomPlaceId = null;
 
 //win screen with congrats text and bkg-img showing 
 axios.get('http://localhost:4004/api/winscreen')
     .then((res) => {
         const { potatoName, randomPlace } = res.data;
+        randomPlaceId = randomPlace.places_id;
         console.log(randomPlace)
         winHeader.innerHTML = `<h3>YAY! You helped ${potatoName} Potato go ${randomPlace.places_name}! </h3>
         <style> body {
@@ -21,7 +22,7 @@ axios.get('http://localhost:4004/api/winscreen')
 axios.get('http://localhost:4004/api/gameAccessories')
     .then((res) => {
         const sqlAccessories = res.data;
-        correctArr = sqlAccessories.filter(element => element.places_id === randomPlace.places_id);
+        correctArr = sqlAccessories.filter(element => element.places_id === randomPlaceId);
         correctArr = correctArr.map(element => element.accessories_url)
         console.log(correctArr)
         correctArr.forEach((accessories) => {
